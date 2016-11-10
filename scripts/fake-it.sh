0d52485a4d69a182e8f75626414ca0e037c1eb44
Bill Morris <wboykinm@geosprocket.com>
2016-11-10 15:58:51 -0500

    more metadata in output filename

--- scripts/fake-it.sh
+++ scripts/fake-it.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #
 # Easy to use script to run neural-style
-#
+# USAGE: ./fake-it.sh <content image filename> <style image filename> <output image width>
+
+DATE=`date +%Y_%m_%d`
 
 if [ $# -lt 2 ]
   then
-    echo "USAGE: fake-it.sh content-image style-image"
+    echo "USAGE: fake-it.sh content-image style-image image-width"
     echo "Current available contents: "
     for i in `ls contents`; do echo -e "\t${i}"; done
     echo "Current available styles: "
     for i in `ls styles`; do echo -e "\t${i}"; done
-	echo "Example: fake-it.sh goldengate.jpg vangogh.jpg"
+	echo "Example: fake-it.sh demo/goldengate.jpg vangogh.jpg 512"
 	echo "To add more contents or styles, simply add them to the folders above"
     exit 1
 fi
@@ -19,8 +21,8 @@ fi
 if groups $USER | grep &>/dev/null '\bdocker\b'; then SU=""
 else SU="sudo"; fi
 
-output_name="output/${1%.*}_by_${2%.*}.png"
-time $SU nvidia-docker run --rm -v $(pwd):/images albarji/neural-style -backend cudnn -cudnn_autotune -normalize_gradients -init image -content_weight 200 -style_weight 300 -content_image contents/$1 -style_image styles/$2 -output_image $output_name -image_size $3
+output_name="output/${1%.*}_by_${2%.*}_${3}px_$DATE.png"
+time $SU nvidia-docker run --rm -v $(pwd):/images albarji/neural-style -backend cudnn -cudnn_autotune -normalize_gradients -init image -content_weight 200 -style_weight 500 -content_image contents/$1 -style_image styles/$2 -output_image $output_name -image_size $3 -save_iter 0
 
 
 
