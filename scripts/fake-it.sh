73c5626497783d4ff7c3cc2167af3a451372c0e0
Bill Morris <wboykinm@geosprocket.com>
2016-11-11 12:10:12 -0500

    slim config for start script

--- scripts/fake-it.sh
+++ scripts/fake-it.sh
@@ -22,7 +22,7 @@ if groups $USER | grep &>/dev/null '\bdocker\b'; then SU=""
 else SU="sudo"; fi
 
 output_name="output/${1%.*}_by_${2%.*}_${3}px_$DATE.png"
-time $SU nvidia-docker run --rm -v $(pwd):/images albarji/neural-style -backend cudnn -cudnn_autotune -normalize_gradients -init image -num_iterations 500 -content_weight 200 -style_weight 400 -content_image contents/$4/$1 -style_image styles/$2 -output_image $output_name -image_size $3
+time $SU nvidia-docker run --rm -v $(pwd):/images albarji/neural-style -backend cudnn -cudnn_autotune -normalize_gradients -init image -num_iterations 100 -content_weight 200 -style_weight 400 -content_image contents/$4/$1 -style_image styles/$2 -output_image $output_name -image_size $3
 
 
 
